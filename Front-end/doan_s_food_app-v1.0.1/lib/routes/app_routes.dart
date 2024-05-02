c146d69ff7ae73c90de7f9dbc52f7aef50837072
rauluoc <21521086@gm.uit.edu.vn>
2024-05-02 15:10:28 +0700

    Decompress

---
+++ Front-end/doan_s_food_app-v1.0.1/lib/routes/app_routes.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import '../core/app_export.dart';
+import '../presentation/detail_profile_admin_screen/detail_profile_admin_screen.dart'; // ignore_for_file: must_be_immutable
+
+// ignore_for_file: must_be_immutable
+class AppRoutes {
+  static const String detailProfileAdminScreen = '/detail_profile_admin_screen';
+
+  static const String initialRoute = '/initialRoute';
+
+  static Map<String, WidgetBuilder> routes = {
+    detailProfileAdminScreen: (context) => DetailProfileAdminScreen(),
+    initialRoute: (context) => DetailProfileAdminScreen()
+  };
+}
