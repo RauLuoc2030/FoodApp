2e155fc5fdbe4ce30231b5a31926531c57f71cbc
rauluoc <21521086@gm.uit.edu.vn>
2024-07-07 19:05:46 +0700

    refactor: Update MealPlan model with nullable properties and add MealPlanRecipes collection

--- Back-end/Models/Bang/mealplan.cs
+++ Back-end/Models/Bang/mealplan.cs
@@ -1,1 +1,29 @@
-sssss
+﻿using System;
+using System.Collections.Generic;
+
+namespace FoodApp.Models;
+
+public partial class MealPlan
+{
+    public int Id { get; set; }
+
+    public int? NguoiDungId { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Description { get; set; }
+
+    public int? AmountOfMeals { get; set; }
+
+    public int? Length { get; set; }
+
+    public DateTime? StartDate { get; set; }
+
+    public DateTime? EndDate { get; set; }
+
+    public int? TotalMeal { get; set; }
+
+    public virtual ICollection<MealPlanRecipe> MealPlanRecipes { get; set; } = new List<MealPlanRecipe>();
+
+    public virtual NguoiDung? NguoiDung { get; set; }
+}
