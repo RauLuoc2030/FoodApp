20e731f211482a1215fa1e665246091405e7f774
bangtran <89561730+bangtran-17@users.noreply.github.com>
2024-07-07 17:27:06 +0700

    nối data người dùng

---
+++ Back-end/Models/Bang/RecipeNutrition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FoodApp.Models;
+
+public partial class RecipeNutrition
+{
+    public int Id { get; set; }
+
+    public int? RecipeId { get; set; }
+
+    public int? NutritionId { get; set; }
+
+    public double? Value { get; set; }
+
+    public virtual NutritionInfo? Nutrition { get; set; }
+
+    public virtual Recipe? Recipe { get; set; }
+}
