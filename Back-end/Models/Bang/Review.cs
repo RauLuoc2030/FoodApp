fedd636d90b0146b4f2d011829cea4cde0a802d3
Kimanhvo278 <kimanhvo2783@gmail.com>
2024-07-07 17:13:59 +0700

    Meal plan review

---
+++ Back-end/Models/Bang/Review.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FoodApp.Models;
+
+public partial class Review
+{
+    public int Id { get; set; }
+
+    public int? RecipeId { get; set; }
+
+    public int? NguoiDungId { get; set; }
+
+    public string? Content { get; set; }
+
+    public int? LikeCount { get; set; }
+
+    public string? TrangThaiDuyet { get; set; }
+
+    public virtual ICollection<Comment> Comments { get; set; } = new List<Comment>();
+
+    public virtual NguoiDung? NguoiDung { get; set; }
+
+    public virtual Recipe? Recipe { get; set; }
+}
